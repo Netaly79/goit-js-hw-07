08fbf1e26c556e9e2cc4d80c363cb968f061ad95
Nataliia Karpenko <n.karpenko@systemomega.com>
2024-04-21 21:15:30 +0200

    task6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.getElementById('controls');
+const input = controls.querySelector('input');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+let amount = 0;
+
+function createBoxes() {
+  amount = Number(input.value);
+  if (amount < 1 || amount > 100) {
+    return;
+  }
+
+  destroyBoxes();
+
+  for (let i = 0; i < amount; i++) {
+    const boxSize = 30 + i * 10;
+    const box = document.createElement('div');
+    box.style.minWidth = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+
+  input.value = '';
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
